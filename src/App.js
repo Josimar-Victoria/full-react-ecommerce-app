5034bfde99a7b01a7af9a04378e3894dafb73cc7
Josimar-Victoria <josimarvictoria968@gmail.com>
2022-12-15 21:07:17 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,58 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './App.css'
+import Layout from './components/Layout'
+import About from './pages/About'
+import Blog from './pages/Blog'
+import Cart from './pages/Cart'
+import Checkout from './pages/Checkout'
+import CompareProduct from './pages/CompareProduct'
+import Contact from './pages/Contact'
+import Forgotpassword from './pages/Forgotpassword'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import OurStore from './pages/OurStore'
+import PrivacyPolicy from './pages/PrivacyPolicy'
+import RefundPloicy from './pages/RefundPloicy'
+import Resetpassword from './pages/Resetpassword'
+import ShippingPolicy from './pages/ShippingPolicy'
+import Signup from './pages/Signup'
+import SingleBlog from './pages/SingleBlog'
+import SingleProduct from './pages/SingleProduct'
+import TermAndContions from './pages/TermAndContions'
+import Wishlist from './pages/Wishlist'
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div className='App'>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path='about' element={<About />} />
+            <Route path='contact' element={<Contact />} />
+            <Route path='product' element={<OurStore />} />
+            <Route path='product/:id' element={<SingleProduct />} />
+            <Route path='blogs' element={<Blog />} />
+            <Route path='blog/:id' element={<SingleBlog />} />
+            <Route path='cart' element={<Cart />} />
+            <Route path='checkout' element={<Checkout />} />
+            <Route path='compare-product' element={<CompareProduct />} />
+            <Route path='wishlist' element={<Wishlist />} />
+            <Route path='login' element={<Login />} />
+            <Route path='forgot-password' element={<Forgotpassword />} />
+            <Route path='signup' element={<Signup />} />
+            <Route path='reset-password' element={<Resetpassword />} />
+            <Route path='privacy-policy' element={<PrivacyPolicy />} />
+            <Route path='refund-policy' element={<RefundPloicy />} />
+            <Route path='shipping-policy' element={<ShippingPolicy />} />
+            <Route path='term-conditions' element={<TermAndContions />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/utils/Data.js
@@ -0,0 +1,35 @@
+import service from '../images/service.png'
+import service02 from '../images/service-02.png'
+import service03 from '../images/service-03.png'
+import service04 from '../images/service-04.png'
+import service05 from '../images/service-05.png'
+
+
+export const services = [
+  {
+    title: 'Free Shipping',
+    tagline: 'From all orders over $5',
+    image: service
+  },
+
+  {
+    title: 'Daily Surprise Offers',
+    tagline: 'Save upto 25% off',
+    image: service02
+  },
+  {
+    title: 'Support 24/7',
+    tagline: 'Shop with an expert',
+    image: service03
+  },
+  {
+    title: 'Affordable Prices',
+    tagline: 'Get Factory Default Price',
+    image: service04
+  },
+  {
+    title: 'Secure Payments',
+    tagline: '100% Protected Payment',
+    image: service05
+  }
+]
